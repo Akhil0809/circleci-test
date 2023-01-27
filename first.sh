bd74bab7b5f1753aa1375cda7b91a140e242dfdd
Akhil0809 <47007823+Akhil0809@users.noreply.github.com>
2023-01-27 16:52:22 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region $AWS_DEFAULT_REGION
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 310961312410.dkr.ecr.ap-south-1.amazonaws.com
 docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$(echo $CIRCLE_SHA1 | head -c7)
 docker images
