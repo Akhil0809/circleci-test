2fd655674fc0faca12eb95c9fbb14f2680a076c0
Akhil0809 <47007823+Akhil0809@users.noreply.github.com>
2023-01-27 18:35:25 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 310961312410.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$(echo $CIRCLE_SHA1 | head -c7)
+docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:latest
 docker images
-docker run -itd --name test-container 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$(echo $CIRCLE_SHA1 | head -c7)
+docker run -itd --name test-container 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:latest
